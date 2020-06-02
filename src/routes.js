fff339ae8e26ba8ac4555ff2a45de703c95d2ebd
Ricardo Brasil <ricardohauus@gmail.com>
2020-06-02 10:54:43 -0300

    Criado o model de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-
+import User from './app/models/User';
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Olá' });
+routes.get('/', async(req, res) => {
+    const user = await User.create({
+        name: 'Ricardo Brasil',
+        email: 'ric@ric.com.br',
+        password_hash: 'b3u12b31%123h123#@!#U%djdksajd',
+    });
+    return res.json(user);
 });
 
 export default routes;
