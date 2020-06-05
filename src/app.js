25feb0778a8b9f5d5408ad478176606abb9634a2
Ricardo Brasil <ricardohauus@gmail.com>
2020-06-05 10:17:00 -0300

    Inserido a liberação para o acesso dos arquivos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 import './database';
+
 class App {
     constructor() {
         this.server = express();
@@ -9,6 +11,10 @@ class App {
     }
     middlewares() {
         this.server.use(express.json());
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+        );
     }
 
     routes() {
