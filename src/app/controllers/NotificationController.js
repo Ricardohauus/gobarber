4e60b75423829313e294f5d7e800df139e3d0d9c
Ricardo Brasil <ricardohauus@gmail.com>
2020-06-15 00:46:13 -0300

    Inserido o método de ler notificação

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,5 +1,6 @@
 import User from '../models/User';
 import Notification from '../schemas/Notification';
+import { id } from 'date-fns/locale';
 
 class NotificationController {
     async index(req, res) {
@@ -17,6 +18,13 @@ class NotificationController {
             .sort({ createdAt: 'desc' })
             .limit(20);
         return res.json(notifications);
+    }
+    async update(req, res) {
+        // const notification = await Notification.findById(req.params.id);
+        const notification = await Notification.findByIdAndUpdate(
+            req.params.id, { read: true }, { new: true }
+        );
+        return res.json(notification);
     }
 }
 
