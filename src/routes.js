cc4ba87d5b896a5c6f130be8ebf8f64b7cd7fa98
Ricardo Brasil <ricardohauus@gmail.com>
2020-06-15 00:36:40 -0300

    Inserido a rota de notificações

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
-
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-import authMiddleware from './app/middlewares/auth';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -26,6 +26,9 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 export default routes;
