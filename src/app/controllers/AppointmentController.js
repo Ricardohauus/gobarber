9d27ebab18345997ab8633f097b74192be8bdedd
Ricardo Brasil <ricardohauus@gmail.com>
2020-06-16 11:39:12 -0300

    Configurado o layout de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -95,10 +95,16 @@ class AppointmentController {
     async delete(req, res) {
         const appointment = await Appointment.findByPk(req.params.id, {
             include: [{
-                model: User,
-                as: 'provider',
-                attributes: ['name', 'email'],
-            }, ],
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email'],
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                },
+            ],
         });
         if (appointment.user_id !== req.userId) {
             return res
@@ -118,7 +124,14 @@ class AppointmentController {
         await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento Cancelado',
-            text: 'Seu agendamento foi cancelado',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(appointment.date, "'dia' dd 'de' MMMM',às' H:mm'h'", {
+                    locale: pt,
+                }),
+            },
         });
         return res.json(appointment);
     }
