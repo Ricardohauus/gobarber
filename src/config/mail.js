2363bcff816dcc1f96ae3210a2b7eca027d920bb
Ricardo Brasil <ricardohauus@gmail.com>
2020-06-16 00:50:39 -0300

    Inserido as configurações do email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'c9e37f6b2e089d',
+        pass: '848141e1baa9f5',
+    },
+    default: {
+        from: ' Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
